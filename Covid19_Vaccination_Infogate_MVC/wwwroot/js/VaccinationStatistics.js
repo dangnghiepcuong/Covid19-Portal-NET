4a1d5e41b09a355f51a0935bcaea44f29757a6f7
Đặng Nghiệp Cường <20520418@gm.uit.edu.vn>
2022-12-09 06:01:52 +0700

    Project Creation

---
+++ Covid19_Vaccination_Infogate_MVC/wwwroot/js/VaccinationStatistics.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    // LOAD FRONT END DATA
+    menu_title = '<a href="VaccinationSatistics.php">Thống kê số liệu tiêm chủng</a>'
+    $('#function-navigation-bar-title').html(menu_title)
+
+    homepage = '<a href="HomepageORG.php">Trang chủ</a>'
+    $('#homepage-path').html(homepage)
+
+    subpage = '<a href="VaccinationSatistics.php">Thống kê</a>'
+    $('#subpage-path').html(subpage)
+
+    selected_function = '<a href="VaccinationSatistics.php">Thống kê số liệu tiêm chủng</a>'
+    $('#selected-function-path').html(selected_function)
+    // END LOAD FRONT END DATA
+})
