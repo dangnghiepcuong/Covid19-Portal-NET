eb671c4d04a2204300ffdd592191282fccc0c19d
TNQT14 <87488896+TNQT14@users.noreply.github.com>
2022-12-14 14:49:26 +0700

    Object

---
+++ Covid19_Vaccination_Infogate_MVC/Models/Injection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Covid19_Vaccination_Infogate_MVC.Models
+{
+    public class Injection
+    {
+        private Citizen citizen;
+        private int InjNO;
+        private Schedule Sched;
+        private String DoseType;
+
+        //Constructor
+        public Injection()
+        {
+            this.citizen = new Citizen();
+            InjNO = -1;
+            Sched = new Schedule();
+            DoseType = "";
+        }
+
+        //Getter, setter
+        public Citizen Citizen { get => citizen; set => citizen = value; }
+        public int InjNO1 { get => InjNO; set => InjNO = value; }
+        public Schedule Sched1 { get => Sched; set => Sched = value; }
+        public string DoseType1 { get => DoseType; set => DoseType = value; }
+    }
+}
---
+++ Covid19_Vaccination_Infogate_MVC/Models/Organization.cs
@@ -0,0 +1,31 @@
+﻿namespace Covid19_Vaccination_Infogate_MVC.Models
+{
+    public class Organization
+    {
+        private string ID;
+        private string Name;
+        private string ProvinceName;
+        private string DistrictName;
+        private string TownName;
+        private string Street;
+
+        //Constructor
+        public Organization()
+        {
+            ID = "";
+            Name = "";
+            ProvinceName = "";
+            DistrictName = "";
+            TownName = "";
+            Street = "";
+        }
+
+        //Getter, Setter
+        public string ID1 { get => ID; set => ID = value; }
+        public string Name1 { get => Name; set => Name = value; }
+        public string ProvinceName1 { get => ProvinceName; set => ProvinceName = value; }
+        public string DistrictName1 { get => DistrictName; set => DistrictName = value; }
+        public string TownName1 { get => TownName; set => TownName = value; }
+        public string Street1 { get => Street; set => Street = value; }
+    }
+}
