f920665cc43445b63db676261b8bb2a94845b4a3
TNQT14 <87488896+TNQT14@users.noreply.github.com>
2022-12-24 14:48:14 +0700

    Update property

--- Covid19_Vaccination_Infogate_MVC/Models/Register.cs
+++ Covid19_Vaccination_Infogate_MVC/Models/Register.cs
@@ -4,35 +4,35 @@ namespace Covid19_Vaccination_Infogate_MVC.Models
 {
     public class Register
     {
-        private Citizen Citizen;
-        private Schedule Sched;
-        private int Time;
-        private int NO;
-        private int Status;
-        private Blob Image;
-        private string DoseType;
-        private int ID;
+        private Citizen citizen;
+        private Schedule sched;
+        private int time;
+        private int no;
+        private int status;
+        private Blob image;
+        private string dosetype;
+        private int id;
         
         //Constructor
         public Register()
         {
-            Citizen = new Citizen();
-            Sched = new Schedule();
-            Time = -1;
-            NO = -1;
-            Status = -1;
-            DoseType = "";
-            ID = -1;
+            citizen = new Citizen();
+            sched = new Schedule();
+            time = -1;
+            no = -1;
+            status = -1;
+            dosetype = "";
+            id = -1;
         }
 
         //Getter, Setter
-        public Citizen Citizen1 { get => Citizen; set => Citizen = value; }
-        public Schedule Sched1 { get => Sched; set => Sched = value; }
-        public int Time1 { get => Time; set => Time = value; }
-        public int NO1 { get => NO; set => NO = value; }
-        public int Status1 { get => Status; set => Status = value; }
-        public Blob Image1 { get => Image; set => Image = value; }
-        public string DoseType1 { get => DoseType; set => DoseType = value; }
-        public int ID1 { get => ID; set => ID = value; }
+        public Citizen Citizen { get; set; }
+        public Schedule Sched { get; set; }
+        public int Time { get ; set ; }
+        public int NO { get ; set; }
+        public int Status { get ; set; }
+        public Blob Image { get ; set ; }
+        public string DoseType { get ; set; }
+        public int ID { get ; set; }
     }
 }
--- Covid19_Vaccination_Infogate_MVC/Models/Schedule.cs
+++ Covid19_Vaccination_Infogate_MVC/Models/Schedule.cs
@@ -4,30 +4,42 @@ namespace Covid19_Vaccination_Infogate_MVC.Models
 {
     public class Schedule
     {
-        private String ID;
-        private Organization Org;
-        private DateTime OnDate;
-        private Vaccine Vaccine;
-        private String Serial;
-        private int LimitDay;
-        private int LimitNoon;
-        private int LimitNight;
-        private int DayRegister;
-        private int NoonRegister;
-        private int NightRegister;
+        private String id;
+        private Organization org;
+        private DateTime ondate;
+        private Vaccine vaccine;
+        private String serial;
+        private int limitday;
+        private int limitnoon;
+        private int limitnight;
+        private int dayregister;
+        private int noonregister;
+        private int nightregister;
 
         public Schedule()
         {
-            ID = "";
-            Org = new Organization();
-            Vaccine = new Vaccine();
-            Serial = "";
-            LimitDay = 0;
-            LimitNoon = 0;
-            LimitNight = 0;
-            DayRegister = 0;
-            NoonRegister = 0;
-            NightRegister = 0;
+            id = "";
+            org = new Organization();
+            vaccine = new Vaccine();
+            serial = "";
+            limitday = 0;
+            limitnoon = 0;
+            limitnight = 0;
+            dayregister = 0;
+            noonregister = 0;
+            nightregister = 0;
         }
+
+        public string Id { get; set; }
+        public Organization Org { get; set; }
+        public DateTime Ondate { get ; set ; }
+        public Vaccine Vaccine { get ; set ; }
+        public string Serial { get; set ; }
+        public int Limitday { get ; set; }
+        public int Limitnoon { get ; set; }
+        public int Limitnight { get; set ; }
+        public int Dayregister { get ; set ; }
+        public int Noonregister { get; set; }
+        public int Nightregister { get; set; }
     }
 }
